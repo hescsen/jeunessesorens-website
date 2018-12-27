26d5fd5d7a3d4ca2c3f6cf6596f27348ea2cfe78
hescsen <info@hescsen.com>
2018-12-27 15:34:53 +0100

    Add navbar for mobile

--- js/navbar.js
+++ js/navbar.js
@@ -7,3 +7,19 @@ $(window).on('scroll', function() {
         $('nav').removeClass('nav-white');
     }
 })
+
+function toggleFunction() {
+    if(document.getElementById('navmobile').style.display=="block") {
+        document.getElementById('navmobile').style.display="none";
+    }
+    else {
+        document.getElementById('navmobile').style.display="block";
+    }
+}
+
+var elements = document.getElementsByTagName('a');
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function() {
+    document.getElementById('navmobile').style.display="none";
+  });
+}
